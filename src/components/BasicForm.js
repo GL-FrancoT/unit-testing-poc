c834019ee4d166aafd8a8c029177bd8bcf7cd8b3
Franco Traverso <franco.traverso@globallogic.com>
2022-08-05 20:56:24 -0300

    Add useInput custom hook in order to validate each input

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,45 @@
 import useInput from "../hooks/use-input";
 
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
 const BasicForm = (props) => {
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
   return (
     <form>
       <div className="control-group">
         <div className="form-control">
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" value={firstNameValue} onChange={firstNameChangeHandler} onBlur={firstNameBlurHandler}/>
         </div>
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" value={lastNameValue} onChange={lastNameChangeHandler} onBlur={lastNameBlurHandler}/>
         </div>
       </div>
       <div className="form-control">
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" value={emailValue} onChange={emailChangeHandler} onBlur={emailBlurHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
