660de7c9d990cc8e03c4fb33e7b2d86654de6e65
Franco Traverso <franco.traverso@globallogic.com>
2022-08-05 21:04:30 -0300

    Display an error message below each incorrect input

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -29,24 +29,49 @@ const BasicForm = (props) => {
     reset: resetEmail,
   } = useInput(isEmail);
 
-  const firstNameClasses = firstNameHasError ? 'form-control invalid' : 'form-control'
-  const lastNameClasses = lastNameHasError ? 'form-control invalid' : 'form-control'
-  const emailClasses = emailHasError ? 'form-control invalid' : 'form-control'
+  const firstNameClasses = firstNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const lastNameClasses = lastNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
   return (
     <form>
       <div className="control-group">
         <div className={firstNameClasses}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" value={firstNameValue} onChange={firstNameChangeHandler} onBlur={firstNameBlurHandler}/>
+          <input
+            type="text"
+            id="name"
+            value={firstNameValue}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameHasError && <p>Please enter a first name.</p>}
         </div>
         <div className={lastNameClasses}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" value={lastNameValue} onChange={lastNameChangeHandler} onBlur={lastNameBlurHandler}/>
+          <input
+            type="text"
+            id="name"
+            value={lastNameValue}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameHasError && <p>Please enter a last name.</p>}
         </div>
       </div>
       <div className={emailClasses}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" value={emailValue} onChange={emailChangeHandler} onBlur={emailBlurHandler}/>
+        <input
+          type="text"
+          id="name"
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailHasError && <p>Please enter a valid email address.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
